cbf93aa4af9caaa6af4646b261f39c95e5ae2e67
Caiob77 <caio.beniel55@gmail.com>
2026-03-24 19:58:11 -0400

    feat: add Swagger documentation support with @fastify/swagger and @fastify/swagger-ui

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod
 import Fastify from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { jsonSchemaTransform } from 'fastify-type-provider-zod'
 
 const app = Fastify({
   logger: true
@@ -10,6 +13,26 @@ const app = Fastify({
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'LifeFit API',
+      description: 'API for the LifeFit app',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8081',
+      },
+    ],
+  },
+  transform: jsonSchemaTransform
+})
+
+await app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+}) 
 
 app.withTypeProvider<ZodTypeProvider>().route({
   method: 'GET',
