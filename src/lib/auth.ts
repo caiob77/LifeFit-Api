7878d751229d16d4ee3c8394b48889604da93151
Caiob77 <caio.beniel55@gmail.com>
2026-03-25 19:32:46 -0400

    refactor: move Prisma client initialization to a separate db module and simplify auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,8 @@
 import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { betterAuth } from "better-auth";
 import { openAPI } from "better-auth/plugins";
-
-
-
-const prisma = new PrismaClient({ 
-    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
-});
+import { prisma } from "./db.js";
 
 const auth = betterAuth({
    emailAndPassword: {
