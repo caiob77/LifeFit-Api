41a8a8dfca0356cf8d0d5cd3524d0066fb7e31d8
Caiob77 <caio.beniel55@gmail.com>
2026-03-26 12:55:29 -0400

    feat: add .env.example file and configure authentication with Google social providers

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,8 +5,12 @@ import { openAPI } from "better-auth/plugins";
 import { prisma } from "./db.js";
 
 const auth = betterAuth({
-   emailAndPassword: {
-    enabled: true,
+   baseURL: process.env.BETTER_AUTH_URL as string,
+   socialProviders: {
+      google: { 
+          clientId: process.env.GOOGLE_CLIENT_ID as string, 
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+      }, 
    },
    database: prismaAdapter(prisma, {
     provider: "postgresql",
